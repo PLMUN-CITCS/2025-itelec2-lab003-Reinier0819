e776e59dd4ad2ecdc67f5b4c5f0464378c7b2251
Reinier Malolos <malolosreinier_bsit@plmun.edu.ph>
2025-03-18 00:46:05 +0800

    Submitting Python Week 02 - Session 01 - Exercise 01

---
+++ exercise_01.py
@@ -0,0 +1,30 @@
+# Reinier Malolos
+# ITELEC2
+# Laboratory #03 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+count = 10
+Count = 15
+decimal_value = 3.14
+
+message = "Hello, Python!"
+
+is_active = True
+
+result = None
+
+total_count = 20
+
+print("Integer (count):", count)
+print("Integer (Count):", Count)
+print("Integer (total_count):", total_count)
+print("Decimal:", decimal_value)
+print("Text:", message)
+print("Boolean:", is_active)
+print("None Value:", result)
+
+
+
+
+
+
